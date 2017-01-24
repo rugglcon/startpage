44b27d9150af655dd2199b9b4777875a0cc9762a
Connor Ruggles <cruggles@iastate.edu>
2017-01-24 16:50:23 -0600

    changed to use weather underground icons

--- js/index.js
+++ js/index.js
@@ -10,63 +10,68 @@ $(document).ready(function() {
 	function handle_weather(parsed_json) {
 		var location = parsed_json.location.city;
 		var tempf = parsed_json.current_observation.temp_f;
-		$('#weather_temp').html("Current temperature: " + tempf + '&deg');
-		$('#weather_icon').html(parsed_json.current_observation.weather);
+		$('#weather_temp').html('Current temperature: ' + tempf + '&deg');
+		var condition = parsed_json.current_observation.weather;
+		$('#weather_icon').html(condition);
 		var winddir = parsed_json.current_observation.wind_dir.toLowerCase();
-		$('#wind_icon').html('<i class=\"wi wi-wind from-' + winddir + '\"></i>');
-		//switch(parsed_json.current_observation.wind_dir) {
-			//case 'NNW':
-				//$('#wind_icon').html('<i class="wi wi-wind from-nnw"></i>');
-				//break;
-			//case 'NNE':
-				//$('#wind_icon').html('<i class="wi wi-wind from-nne"></i>');
-				//break;
-			//case 'NE':
-				//$('#wind_icon').html('<i class="wi wi-wind from-ne"></i>');
-				//break;
-			//case 'NW':
-				//$('#wind_icon').html('<i class="wi wi-wind from-nw"></i>');
-				//break;
-			//case 'N':
-				//$('#wind_icon').html('<i class="wi wi-wind from-n"></i>');
-				//break;
-			//case 'W':
-				//$('#wind_icon').html('<i class="wi wi-wind from-w"></i>');
-				//break;
-			//case 'E':
-				//$('#wind_icon').html('<i class="wi wi-wind from-e"></i>');
-				//break;
-			//case 'S':
-				//$('#wind_icon').html('<i class="wi wi-wind from-s"></i>');
-				//break;
-			//case 'SE':
-				//$('#wind_icon').html('<i class="wi wi-wind from-se"></i>');
-				//break;
-			//case 'SW':
-				//$('#wind_icon').html('<i class="wi wi-wind from-sw"></i>');
-				//break;
-			//case 'SSW':
-				//$('#wind_icon').html('<i class="wi wi-wind from-ssw"></i>');
-				//break;
-			//case 'SSE':
-				//$('#wind_icon').html('<i class="wi wi-wind from-sse"></i>');
-				//break;
-			//case 'WNW':
-				//$('#wind_icon').html('<i class="wi wi-wind from-wnw"></i>');
-				//break;
-			//case 'ENE':
-				//$('#wind_icon').html('<i class="wi wi-wind from-ene"></i>');
-				//break;
-			//case 'WSW':
-				//$('#wind_icon').html('<i class="wi wi-wind from-wsw"></i>');
-				//break;
-			//case 'ESE':
-				//$('#wind_mph').html(parsed_json.current_observation.wind_mph + 'mph');
-				//$('#wind_icon').html('<i class="wi wi-wind from-ese"></i>');
-				//break;
-			//default:
-				//$('#wind_icon').html(parsed_json.current_observation.wind_dir);
-				//break;
-		//}
+		switch(condition.toLowerCase()) {
+			case 'overcast':
+				condition = 'cloudy';
+				break;
+			case 'scattered clouds':
+				condition = 'partlycloudy';
+				break;
+			case 'thunderstorm':
+				condition = 'tstorms';
+				break;
+			case 'thunderstorms':
+				condition = 'tstorms';
+				break;
+			case 'freezing rain':
+				condition = 'sleet';
+				break;
+			case 'partly sunny':
+				condition = 'partlysunny';
+				break;
+			case 'partly cloudy':
+				condition = 'partlycloudy';
+				break;
+			case 'mostly sunny':
+				condition = 'mostlysunny';
+				break;
+			case 'mostly cloudy':
+				condition = 'mostlycloudy';
+				break;
+			case 'haze':
+				condition = 'hazy';
+				break;
+			case 'chance of a thunderstorm':
+				condition = 'chancetstorms';
+				break;
+			case 'chance of thunderstorms':
+				condition = 'chancetstorms';
+				break;
+			case 'chance of snow':
+				condition = 'chancesnow';
+				break;
+			case 'chance of sleet':
+				condition = 'chancesleet';
+				break;
+			case 'chance of freezing rain':
+				condition = 'chancesleet';
+				break;
+			case 'chance rain':
+				condition = 'chancerain';
+				break;
+			case 'chance of rain':
+				condition = 'chancerain';
+				break;
+			case 'chance of flurries':
+				condition = 'chanceflurries';
+				break;
+			default:
+				break;
+		}
+		$('#wind_icon').html('<img src=\"https://icons.wxug.com/i/c/i/' + condition + '.gif\"/>');
 	}
 });
