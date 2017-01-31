a209c82a089393a4d1d2514ae8db941c419f07ec
Connor Ruggles <cruggles@iastate.edu>
2017-01-31 12:18:53 -0600

    simplify logic for weather icon

--- js/index.js
+++ js/index.js
@@ -10,6 +10,20 @@ $(document).ready(function() {
 		}
 	});
 
+	$.ajax({
+		url: "https://api.wunderground.com/api/5040da7e655d1412/geolookup/forecast/q/IA/Ames.json",
+		dataType: "jsonp",
+		success: function(data) {
+			handle_forecast(data);
+		},
+		error: function(data) {
+			$('#weather').html('Error: ' + data);
+		}
+	});
+
+	function handle_forecast(forecast_data) {
+	}
+
 	function handle_weather(parsed_json) {
 		var tempf = parsed_json.current_observation.temp_f;
 		$('#weather_temp').html('Current temperature: ' + tempf + '&deg');
@@ -17,68 +31,7 @@ $(document).ready(function() {
 		var winddir = parsed_json.current_observation.wind_dir;
 		var windmph = parsed_json.current_observation.wind_mph;
 		$('#wind_mph').html('Wind is ' + windmph + ' mph coming from ' + winddir);
-		switch(condition.toLowerCase()) {
-			case 'overcast':
-				condition = 'cloudy';
-				break;
-			case 'scattered clouds':
-				condition = 'partlycloudy';
-				break;
-			case 'thunderstorm':
-				condition = 'tstorms';
-				break;
-			case 'thunderstorms':
-				condition = 'tstorms';
-				break;
-			case 'freezing rain':
-				condition = 'sleet';
-				break;
-			case 'partly sunny':
-				condition = 'partlysunny';
-				break;
-			case 'partly cloudy':
-				condition = 'partlycloudy';
-				break;
-			case 'mostly sunny':
-				condition = 'mostlysunny';
-				break;
-			case 'mostly cloudy':
-				condition = 'mostlycloudy';
-				break;
-			case 'haze':
-				condition = 'hazy';
-				break;
-			case 'chance of a thunderstorm':
-				condition = 'chancetstorms';
-				break;
-			case 'chance of thunderstorms':
-				condition = 'chancetstorms';
-				break;
-			case 'chance of snow':
-				condition = 'chancesnow';
-				break;
-			case 'chance of sleet':
-				condition = 'chancesleet';
-				break;
-			case 'chance of freezing rain':
-				condition = 'chancesleet';
-				break;
-			case 'chance rain':
-				condition = 'chancerain';
-				break;
-			case 'chance of rain':
-				condition = 'chancerain';
-				break;
-			case 'chance of flurries':
-				condition = 'chanceflurries';
-				break;
-			case 'light snow':
-				condition = 'snow';
-				break;
-			default:
-				condition = condition.toLowerCase();
-				break;
-		}
-		$('#weather_icon').html('<img src=\"https://icons.wxug.com/i/c/i/' + condition + '.gif\"/>');
+		var condition_icon = parsed_json.current_observation.icon_url;
+		$('#weather_icon').html('<img src=\"' + condition_icon.replace('i/c/k', 'i/c/i') + '\"/>');
 	}
 });
