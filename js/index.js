4aaff7321a565b95eefb78c82b28bf7bd9a7afa5
Connor Ruggles <cruggles@iastate.edu>
2017-01-31 22:46:50 -0600

    started adding forecast logic

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 			handle_weather(data);
 		},
 		error: function(data) {
-			$('#weather_temp').html('Error: ' + data);
+			$('#cur_weather_temp').html('Error: ' + data);
 		}
 	});
 
@@ -22,16 +22,29 @@ $(document).ready(function() {
 	});
 
 	function handle_forecast(forecast_data) {
+		var periods = forecast_data.forecast.simpleforecast.forecastday;
+		for(var i = 0; i < periods.length; i++) {
+			var forecast_div = $('<div></div>');
+			var day = $('<p class="center"></p>').text(periods[i].date.weekday);
+			var high = periods[i].high.fahrenheit;
+			var low = periods[i].low.fahrenheit;
+			var avewind = periods[i].avewind.mph;
+			var avewind_dir = periods[i].avewind.dir;
+			var temp = $('<p class="center"></p>').text('High: ' + high + ' Low: ' + low);
+			$(forecast_div).css('width', '25%');
+			$(forecast_div).append(day, temp);
+			$('#forecast').append(forecast_div);
+		}
 	}
 
 	function handle_weather(parsed_json) {
 		var tempf = parsed_json.current_observation.temp_f;
-		$('#weather_temp').html('Current temperature: ' + tempf + '&deg');
+		$('#cur_weather_temp').html('Current temperature: ' + tempf + '&deg');
 		var condition = parsed_json.current_observation.weather;
 		var winddir = parsed_json.current_observation.wind_dir;
 		var windmph = parsed_json.current_observation.wind_mph;
-		$('#wind_mph').html('Wind is ' + windmph + ' mph coming from ' + winddir);
+		$('#cur_wind_mph').html('Wind is ' + windmph + ' mph coming from ' + winddir);
 		var condition_icon = parsed_json.current_observation.icon_url;
-		$('#weather_icon').html('<img src=\"' + condition_icon.replace('i/c/k', 'i/c/i') + '\"/>');
+		$('#cur_weather_icon').html('<img src=\"' + condition_icon.replace('i/c/k/', 'i/c/i/') + '\"/>');
 	}
 });
