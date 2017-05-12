f35c8916127f8c5d4cce0205730612857ca079cd
Connor Ruggles <cruggles@iastate.edu>
2017-05-12 13:42:41 -0500

    Get layout correct

--- js/index.js
+++ js/index.js
@@ -1,42 +1,40 @@
 $(document).ready(function() {
 
 
-	// $.ajax({
-	// 	url: "https://api.wunderground.com/api/5040da7e655d1412/geolookup/conditions/q/IA/Ames.json",
-	// 	dataType: "jsonp",
-	// 	success: function(data) {
-	// 		handle_weather(data);
-	// 	},
-	// 	error: function(data) {
-	// 		$('#cur_weather_temp').html('Error: ' + data);
-	// 	}
-	// });
+	$.ajax({
+		url: "https://api.wunderground.com/api/5040da7e655d1412/geolookup/conditions/q/IA/Ames.json",
+		dataType: "jsonp",
+		success: function(data) {
+			handle_weather(data);
+		},
+		error: function(data) {
+			$('#cur_weather_temp').html('Error: ' + data);
+		}
+	});
 
-	// $.ajax({
-	// 	url: "https://api.wunderground.com/api/5040da7e655d1412/geolookup/forecast/q/IA/Ames.json",
-	// 	dataType: "jsonp",
-	// 	success: function(data) {
-	// 		handle_forecast(data);
-	// 	},
-	// 	error: function(data) {
-	// 		$('#weather').html('Error: ' + data);
-	// 	}
-	// });
+	$.ajax({
+		url: "https://api.wunderground.com/api/5040da7e655d1412/geolookup/forecast/q/IA/Ames.json",
+		dataType: "jsonp",
+		success: function(data) {
+			handle_forecast(data);
+		},
+		error: function(data) {
+			$('#weather').html('Error: ' + data);
+		}
+	});
 
 	function handle_forecast(forecast_data) {
 		var periods = forecast_data.forecast.simpleforecast.forecastday;
 		for(var i = 1; i < periods.length - 1; i++) {
-			var forecast_div = $('<div class="border_top border_right"></div>');
-			var day = $('<p></p>').text(periods[i].date.weekday);
-			var icon = $('<img src="' + periods[i].icon_url.replace('i/c/k/', 'i/c/i/') + '"/>');
+			var forecast_div = $('<div class="border_top"></div>');
 			var high = periods[i].high.fahrenheit;
 			var low = periods[i].low.fahrenheit;
+			var day = $('<p class="small-bottom"></p>').text(periods[i].date.weekday + ', High: ' + high + ' Low: ' + low);
+			var icon = $('<img src="' + periods[i].icon_url.replace('i/c/k/', 'i/c/i/') + '"/>');
 			var avewind = periods[i].avewind.mph;
 			var avewind_dir = periods[i].avewind.dir;
-			var temp = $('<p></p>').text('High: ' + high + ' Low: ' + low);
-			var wind = $('<p></p>').text('Ave wind: ' + avewind + ' mph');
-			$(forecast_div).css({'float':'left'});
-			$(forecast_div).append(day, icon, temp, wind);
+			var wind = $('<p class="small-top"></p>').text('Ave wind: ' + avewind + ' mph');
+			$(forecast_div).append(day, icon, wind);
 			$('#forecast').append(forecast_div);
 		}
 	}
